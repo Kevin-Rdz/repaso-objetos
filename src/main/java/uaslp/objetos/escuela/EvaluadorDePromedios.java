d73cdae0a7291bbadf15d010163ddfb08effaf29
Kevin-Rdz <evrick09@gmail.com>
2022-05-23 23:33:14 -0500

    Quinto Ejercicio: 23 min

--- src/main/java/uaslp/objetos/escuela/EvaluadorDePromedios.java
+++ src/main/java/uaslp/objetos/escuela/EvaluadorDePromedios.java
@@ -3,10 +3,11 @@ package uaslp.objetos.escuela;
 import java.util.List;
 
 public class EvaluadorDePromedios {
-
+    private double promedio;
+    private double sum;
     public double evalua(List<Double> calificaciones){
-        double sum = calificaciones.stream().reduce(0.0, Double::sum);
-
-        return sum / calificaciones.size();
+        sum = calificaciones.stream().reduce(0.0, Double::sum);
+        promedio = sum / calificaciones.size();
+        return promedio;
     }
 }
